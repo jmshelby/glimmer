cedb68761965e4a29ab8548f39f280e01fff0820
Jake Shelby <1891693+jmshelby@users.noreply.github.com>
2025-06-24 23:22:40 -0600

    Config: closer to source patterns

--- src/glimmer/app/config.clj
+++ src/glimmer/app/config.clj
@@ -18,16 +18,28 @@
                     ensure-keyword))))
 
 (defn get-cloud-params
+  "Get base params from AWS SSM for the current env & app pair."
   ([] (get-cloud-params (env) (app)))
   ([env app]
+   (when (not (or env app))
+     (throw (ex-info "Glimmer app param configuration not setup, are you running this app with the correct configurations?"
+                     {:configuration {:env env
+                                      :app app}})))
    (->> (ion/get-params
           {:path (format "/datomic-shared/%s/%s/" (name env) app)})
         (map #(vector (keyword (first %)) (second %)))
         (into {}))))
 
+(defn get-params
+  "Current in-memory environment params, for this app" []
+  (when (= ::empty @STATE)
+    ;; Lazy load params when state is empty
+    ;; with existing env and app name
+    (clojure.core/reset! STATE (get-cloud-params)))
+  @STATE)
+
 (defn get
-  ([] (when (= ::empty @STATE)
-        (clojure.core/reset! STATE (get-cloud-params)))
-      @STATE)
-  ([key] (clojure.core/get (get) key)))
+  "Main method to get configuration parameters"
+  ([] (get-params))
+  ([key] (clojure.core/get (get-params) key)))
 
